46f9fc9846322fdd60a35755758377856b3a3fe7
JS Nerd <shaikhdanish1710@gmail.com>
2024-07-18 00:05:52 +0530

    Counter app updated with condition to not let counter go below 0

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import { useState } from 'react'
 
 function App() {
   
+  const [count,setCount]=useState(0);
 
+
+  function Increment(){
+    setCount(count+1);
+  }
+  function Decrement(){
+    if(count>0){
+      setCount(count-1);
+    }
+  }
   return (
     <>
-      <h1>Test</h1>
+      <h1>Counter : {count}</h1>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
     </>
   )
 }
